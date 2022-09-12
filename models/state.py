8b33c8c237abb08a2012b05b329bb705a7855346
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-12 14:11:04 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+""" States Module for HBNB project"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
+import os
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+
+class State(BaseModel, Base):
+    """
+    Class that inherits from BaseModel and Base (respect the order)
+    Attributes:
+        __tablename__ : represents the table name
+        name: input name
+    """
+     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if (os.getenv("HBNB_TYPE_STORAGE") != "db"):
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city = []
+            for obj in list(models.storage.all(City).values()):
+                if obj.state_id == self.id:
+                    city.append(obj)
+            return city
