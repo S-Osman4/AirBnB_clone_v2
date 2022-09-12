5864c20d798dd26cdf569d64242d4d122329a57e
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-12 15:13:21 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -17,7 +17,7 @@ class State(BaseModel, Base):
         __tablename__ : represents the table name
         name: input name
     """
-     __tablename__ = "states"
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
 
