2a9a3adf7937a6d3581b6ea28d796501428c08a0
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-14 14:31:21 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
