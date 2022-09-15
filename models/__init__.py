1f0a921e9a695c95fc555ccd223c9b3a4d8a97b1
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-09-15 17:39:46 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -15,5 +15,5 @@ if (os.getenv("HBNB_TYPE_STORAGE") == "db"):
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-   storage = FileStorage()
+    storage = FileStorage()
 storage.reload()
